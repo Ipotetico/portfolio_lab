3c7bf4ac863ea5cb7d547f96d8ab258877672a63
Marcin Zbrożek <m.zbrozek@kiciu.ad.agencjakore.pl>
2020-10-21 12:42:24 +0200

    Updates

---
+++ precache-manifest.a856146c0b2d9e73cde84237d00bdc15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4731fc51257d18829cc70b0b63fd6266",
+    "url": "/portfolio_lab/index.html"
+  },
+  {
+    "revision": "f02bf8c0971b4a954b75",
+    "url": "/portfolio_lab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b75632239c3c061aacb",
+    "url": "/portfolio_lab/static/js/2.cf51cfa4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/portfolio_lab/static/js/2.cf51cfa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02bf8c0971b4a954b75",
+    "url": "/portfolio_lab/static/js/main.c38946ed.chunk.js"
+  },
+  {
+    "revision": "a4f626bb41c890834981",
+    "url": "/portfolio_lab/static/js/runtime-main.e5ed9f55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio_lab/static/media/logo.5d5d9eef.svg"
+  }
+]);
